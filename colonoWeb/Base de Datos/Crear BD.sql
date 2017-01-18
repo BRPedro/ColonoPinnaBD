5d88395975ae795a31c4265f154d693557cccac8
Pedro Barrantes Rodríguez <barrantes.r.pedro@gmail.com>
2017-01-18 10:19:21 -0600

    Subir pproyecto

---
+++ colonoWeb/Base de Datos/Crear BD.sql
@@ -0,0 +1,7 @@
+﻿CREATE DATABASE "BDAPTECH2"
+  WITH OWNER = colono14
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Spanish_Spain.1252'
+       LC_CTYPE = 'Spanish_Spain.1252'
+       CONNECTION LIMIT = -1;
